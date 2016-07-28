cc5e27cd467d09988c703f16ea925c7c374a664a
Allen Kim <akim@cetaris.com>
2016-07-28 10:02:54 -0400

    updated

--- app/ng2-ui-components/google-map.component.ts
+++ app/ng2-ui-components/google-map.component.ts
@@ -4,33 +4,80 @@ import { NG2_DIRECTIVES } from 'ng2-ui';
 @Component({
   directives: [ NG2_DIRECTIVES ],
   template: `
-    <div class="container bg-sky title">
+    <div class="container bg-sky title" xmlns="http://www.w3.org/1999/html">
       <h1>Google Map</h1>
     </div>
     <div class="container">
     
-      <h4>Simple Map</h4>
-      Feature is ready, Coming Soon with an example.
-      
-      <h4>Simple Marker</h4>
-      Feature is ready, Coming Soon with an example.
-      
-      <h4>Simple InfoWindow</h4>
-      <ng2-map center="Brampton, Canada" scrollwheel="false">
-        <marker position="Brampton, Canada" draggable="true" (markerClick)="clicked($event)"></marker>
-        <info-window id="iw">
-          lat: [[lat]], lng: [[lng]]
-        </info-window>
-      </ng2-map>
-      Please click a marker to see info window
       <div class="spacer x3"></div>
+      <h4>Simple Map</h4>
       <div>
         <div class="tabs">
-          <div class="tab" (click)="tab=1" [class.selected]="tab===1">html</div>
-          <div class="tab" (click)="tab=2" [class.selected]="tab===2">javascript</div>
+          <div class="tab" (click)="tab4=1" [class.selected]="tab4===1">html</div>
+          <div class="tab" (click)="tab4=2" [class.selected]="tab4===2">javascript</div>
         </div>
         <div class="tab-contents">
-          <div id="html" *ngIf="tab===1">
+          <div *ngIf="tab4===1">
+            <pre><code class="language-markup"
+            >&lt;ng2-map center="Brampton, Canada" scrollwheel="false">&lt;/ng2-map></code></pre>
+          </div>
+          <div *ngIf="tab4===2">
+            <pre><code class="language-markup"
+            >import { Component } from '@angular/core';
+import { NG2_DIRECTIVES } from 'ng2-ui';
+
+@Component({
+  directives: [ NG2_DIRECTIVES ],
+  templateUrl: 'app.html'
+})
+class AppCompoment {}</code></pre>
+          </div>
+        </div>
+      </div>
+          
+      <div class="spacer x3"></div>
+      <h4>Simple Marker</h4>
+      <div>
+        <div class="tabs">
+          <div class="tab" (click)="tab1=1" [class.selected]="tab1===1">html</div>
+          <div class="tab" (click)="tab1=2" [class.selected]="tab1===2">javascript</div>
+        </div>
+        <div class="tab-contents">
+          <div *ngIf="tab1===1">
+            <pre><code class="language-markup">&lt;ng2-map center="Brampton, Canada" >
+&lt;marker position="Brampton, Canada"
+  (markerDragstart)="log($event, 'dragstart')"
+  (markerDragend)="log($event, 'dragend')"
+  draggable="true">&lt;/marker>
+&lt;/ng2-map></code></pre>
+          </div>
+          <div *ngIf="tab1===2">
+            <pre><code class="language-markup"
+            >import { Component } from '@angular/core';
+import { NG2_DIRECTIVES } from 'ng2-ui';
+
+@Component({
+  directives: [ NG2_DIRECTIVES ],
+  templateUrl: 'app.html'
+})
+class AppCompoment {
+  log(marker, str) {
+    console.log('event .... >', marker, str);
+  }
+}</code></pre>
+          </div>
+        </div>
+      </div>
+      
+      <div class="spacer x3"></div>
+      <h4>Simple InfoWindow</h4>
+      <div>
+        <div class="tabs">
+          <div class="tab" (click)="tab2=1" [class.selected]="tab2===1">html</div>
+          <div class="tab" (click)="tab2=2" [class.selected]="tab2===2">javascript</div>
+        </div>
+        <div class="tab-contents">
+          <div id="html" *ngIf="tab2===1">
 <pre><code class="language-markup"
 >&lt;ng2-map center="Brampton, Canada">
   &lt;marker position="Brampton, Canada" draggable="true" (markerClick)="clicked($event)">&lt;/marker>
@@ -39,7 +86,7 @@ import { NG2_DIRECTIVES } from 'ng2-ui';
   &lt;/info-window>
 &lt;/ng2-map></code></pre>
           </div>
-          <div id="javascript" *ngIf="tab===2">
+          <div id="javascript" *ngIf="tab2===2">
 <pre><code class="language-javascript"
 >import { Component } from '@angular/core';
 import { NG2_DIRECTIVES } from 'ng2-ui';
@@ -58,13 +105,60 @@ class AppCompoment {
           </div>
         </div>
       </div>
-      
+     
+      <div class="spacer x3"></div>
       <h4>Markers with *ngFor</h4>
-      Feature is ready, Coming Soon with an example.
+        <ng2-map zoom="13" center="Brampton, Canada">
+          <marker *ngFor="let pos of positions" [position]="pos"></marker>
+        </ng2-map>
+        <button (click)="showRandomMarkers()">Show Random Markers</button>
+      <div>
+        <div class="tabs">
+          <div class="tab" (click)="tab3=1" [class.selected]="tab3===1">html</div>
+          <div class="tab" (click)="tab3=2" [class.selected]="tab3===2">javascript</div>
+        </div>
+        <div class="tab-contents">
+          <div *ngIf="tab3===1">
+            <pre><code class="language-markup"
+>&lt;ng2-map zoom="13" center="Brampton, Canada">
+  &lt;marker *ngFor="let pos of positions" [position]="pos">&lt;/marker>
+&lt;/ng2-map>
+&lt;button (click)="showRandomMarkers()">Show Random Markers&lt;/button></code></pre>
+          </div>
+          <div *ngIf="tab3===2">
+            <pre><code class="language-markup"
+            >import { Component } from '@angular/core';
+import { NG2_DIRECTIVES } from 'ng2-ui';
+
+@Component({
+  directives: [ NG2_DIRECTIVES ],
+  templateUrl: 'app.html'
+})
+class AppCompoment {
+  public positions=[];
+  
+  showRandomMarkers() {
+    let randomLat: number, randomLng: number;
+
+    this.positions = [];
+    for (let i=0; i<9; i++) {
+      randomLat = Math.random() * (43.7399 - 43.7300) + 43.7300;
+      randomLng = Math.random() * (-79.7600 - -79.7699) + -79.7699;
+      this.positions.push([randomLat, randomLng]);
+    }
+  }
+}</code></pre>
+          </div>
+        </div>
+      </div>
     </div>`
 })
 export class GoogleMapComponent {
-  tab: number = 1;
+  positions=[];
+  tab1: number = 1;
+  tab2: number = 1;
+  tab3: number = 1;
+  tab4: number = 1;
   ngAfterViewInit(): void {
     window.scroll(0,0);
   }
@@ -72,5 +166,16 @@ export class GoogleMapComponent {
     marker.map.mapComponent.openInfoWindow('iw', marker, {
       lat: marker.getPosition().lat(), lng: marker.getPosition().lng()
     })
+  }
+
+  showRandomMarkers() {
+    let randomLat: number, randomLng: number;
+
+    this.positions = [];
+    for (let i=0; i<9; i++) {
+      randomLat = Math.random() * (43.7399 - 43.7300) + 43.7300;
+      randomLng = Math.random() * (-79.7600 - -79.7699) + -79.7699;
+      this.positions.push([randomLat, randomLng]);
+    }
   }
 }
