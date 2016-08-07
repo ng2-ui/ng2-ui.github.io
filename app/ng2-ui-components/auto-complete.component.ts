415508052106bde842b236d177907c623c2a8e4f
Allen Kim <allenhwkim@gmail.com>
2016-08-06 20:26:58 -0400

    added tabs for autocomplte

--- app/ng2-ui-components/auto-complete.component.ts
+++ app/ng2-ui-components/auto-complete.component.ts
@@ -11,39 +11,86 @@ import { NG2_DIRECTIVES } from 'ng2-ui';
       <div class="spacer x3"></div>
       <div class="container round-border">
         <div class="title">Simple string of array as source</div>
-        <pre>{{arrayOfStrings | json}}</pre >
         <p><input ng2-auto-complete
           [(ngModel)]="model1"
           [source]="arrayOfStrings"
           placeholder="enter text"/>
         <p><b>model1</b>: {{model1 | json}}
+        <ng2-tab>
+          <div class="tabs">
+             <div index="html">HTML</div>
+             <div index="js">Javascript</div>
+           </div> 
+           <div class="tab-contents">
+             <div contents="html">
 <pre><code class="language-markup"
->&lt;input ng2-auto-complete 
-       [(ngModel)]="model1"
-       [source]="arrayOfStrings"
-       placeholder="enter text"/></code></pre>
+>&lt;input ng2-auto-complete
+          [(ngModel)]="model1"
+          [source]="arrayOfStrings"
+          placeholder="enter text"/></code></pre>
+             </div>
+             <div contents="js">
+<pre><code class="language-javascript"
+>import { Component } from '@angular/core';
+import { NG2_DIRECTIVES } from 'ng2-ui';
+
+@Component({
+  directives: [ NG2_DIRECTIVES ],
+  templateUr: 'app.html'
+})
+export class AppComponent{
+  model1: any;
+  arrayOfStrings: string[] =
+    ["this", "is", "array", "of", "text"];
+}</code></pre>
+             </div>
+           </div>
+        </ng2-tab>
       </div>
       <div class="spacer x4"></div>
       
       <div class="container round-border">
         <div class="title">Simple object of array as source</div>
-        <pre>{{arrayOfKeyValues | json}}</pre> 
         <p><input ng2-auto-complete
           [(ngModel)]="model2"
           [source]="arrayOfKeyValues" 
           placeholder="enter text"/> 
         <p><b>model2</b>: {{model2 | json}}
+        <ng2-tab>
+          <div class="tabs">
+             <div index="html">HTML</div>
+             <div index="js">Javascript</div>
+           </div> 
+           <div class="tab-contents">
+             <div contents="html">
 <pre><code class="language-markup"
 >&lt;input ng2-auto-complete
-       [(ngModel)]="model2"
-       [source]="arrayOfKeyValues" 
-       placeholder="enter text"/> </code></pre>
+        [(ngModel)]="model2"
+        [source]="arrayOfKeyValues" 
+        placeholder="enter text"/></code></pre>
+             </div>
+             <div contents="js">
+<pre><code class="language-javascript"
+>import { Component } from '@angular/core';
+import { NG2_DIRECTIVES } from 'ng2-ui';
+
+@Component({
+  directives: [ NG2_DIRECTIVES ],
+  templateUr: 'app.html'
+})
+export class AppComponent{
+  model2: any;
+  arrayOfKeyValues: any[] =
+    [{id:1, value:'One'}, {id:2, value:'Two'}, {id:3, value:'Three'}, {id:4, value:'Four'}];
+}</code></pre>
+             </div>
+           </div>
+        </ng2-tab>
       </div>
       <div class="spacer x4"></div>
       
       <div class="container round-border">
         <div class="title">Custom object of array as source</div>
-        <pre>{{arrayOfKeyValues2 | json}}</pre>
         <p><input ng2-auto-complete 
           [source]="arrayOfKeyValues2"
           [(ngModel)]="model3"
@@ -51,12 +98,38 @@ import { NG2_DIRECTIVES } from 'ng2-ui';
           value-property-name="key"
           display-property-name="name"/></p>
         <p><b>model3:</b> {{model3 | json}}<br/></p>
+        <ng2-tab>
+          <div class="tabs">
+             <div index="html">HTML</div>
+             <div index="js">Javascript</div>
+           </div> 
+           <div class="tab-contents">
+             <div contents="html">
 <pre><code class="language-markup"
->&lt;input ng2-auto-complete [source]="arrayOfKeyValues2"
-       [(ngModel)]="model3"
-       placeholder="enter text"
-       value-property-name="key"
-       display-property-name="name"/></code></pre>
+>&lt;input ng2-auto-complete 
+          [source]="arrayOfKeyValues2"
+          [(ngModel)]="model3"
+          placeholder="enter text"
+          value-property-name="key"
+          display-property-name="name"/></code></pre>
+             </div>
+             <div contents="js">
+<pre><code class="language-javascript"
+>import { Component } from '@angular/core';
+import { NG2_DIRECTIVES } from 'ng2-ui';
+
+@Component({
+  directives: [ NG2_DIRECTIVES ],
+  templateUr: 'app.html'
+})
+export class AppComponent{
+  model3: any;
+  arrayOfKeyValues2: any[] =
+    [{key:1, name:'Key One'}, {key:2, name:'Key Two'}, {key:3, name:'Key Three'}, {key:4, name:'Key Four'}];
+}</code></pre>
+             </div>
+           </div>
+        </ng2-tab>
       </div>
         
       <h2> Autocomplete From Remote Source </h2>
@@ -72,15 +145,40 @@ import { NG2_DIRECTIVES } from 'ng2-ui';
           display-property-name="formatted_address"
           path-to-data="results"
           min-chars="2" />
-<pre><code class="language-markup"
->&lt;input ng2-auto-complete 
-       [(ngModel)]="model4"
-       placeholder="Enter Address(min. 2 chars)"
-       [source]="googleGeoCode" 
-       display-property-name="formatted_address"
-       path-to-data="results"
-       min-chars="2" /></code></pre>
         <p><b>model4</b>: {{model4 | json}}<br/></p>
+        <ng2-tab>
+          <div class="tabs">
+             <div index="html">HTML</div>
+             <div index="js">Javascript</div>
+           </div> 
+           <div class="tab-contents">
+             <div contents="html">
+<pre><code class="language-markup"
+>&lt;input ng2-auto-complete
+          [(ngModel)]="model4"
+          placeholder="Enter Address(min. 2 chars)"
+          [source]="googleGeoCode" 
+          display-property-name="formatted_address"
+          path-to-data="results"
+          min-chars="2" /></code></pre>
+             </div>
+             <div contents="js">
+<pre><code class="language-javascript"
+>import { Component } from '@angular/core';
+import { NG2_DIRECTIVES } from 'ng2-ui';
+
+@Component({
+  directives: [ NG2_DIRECTIVES ],
+  templateUr: 'app.html'
+})
+export class AppComponent{
+  model4: any;
+
+  googleGeoCode: string = "https://maps.googleapis.com/maps/api/geocode/json?address=:keyword";
+}</code></pre>
+             </div>
+           </div>
+        </ng2-tab>
       </div>
       <div class="spacer x4"></div>
       <div class="spacer x4"></div>
