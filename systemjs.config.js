e3541f12850883e1c34a6cfee145632e03d4d2c7
Allen Kim <allenhwkim@gmail.com>
2016-08-28 00:54:21 -0400

    updated to all ngmodules

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,7 @@
     'rxjs':      'https://npmcdn.com/rxjs@5.0.0-beta.6',
     'ts':        'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
     'typescript': 'https://npmcdn.com/typescript@1.9.0-dev.20160409/lib/typescript.js',
-    'ng2-ui':     'https://npmcdn.com/ng2-ui/dist'
+    'ng2-ui':     'https://npmcdn.com/ng2-ui@0.9.3/dist'
   };
 
   var packages = {
@@ -22,58 +22,6 @@
     rxjs: { defaultExtension: 'js' },
     'ng2-ui': {main: 'index.js', defaultExtension: 'js'}
   };
-
-  // var ngPackageNames = [
-  //   'common',
-  //   'compiler',
-  //   'core',
-  //   'http',
-  //   'platform-browser',
-  //   'platform-browser-dynamic',
-  //   'upgrade',
-  // ];
-  //
-  // // Add map entries for each angular package
-  // // only because we're pinning the version with `ngVer`.
-  // ngPackageNames.forEach(function(pkgName) {
-  //   map['@angular/'+pkgName] = 'https://npmcdn.com/@angular/' + pkgName + ngVer;
-  // });
-  //
-  // // Add package entries for angular packages
-  // ngPackageNames.forEach(function(pkgName) {
-  //
-  //   // Bundled (~40 requests):
-  //   packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-  //
-  //   // Individual files (~300 requests):
-  //   //packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-  // });
-  //
-  // // No umd for router yet
-  // packages['@angular/router'] = { main: 'index.js', defaultExtension: 'js' };
-  //
-  // // Forms not on rc yet
-  // packages['@angular/forms'] = { main: 'index.js', defaultExtension: 'js' };
-  //
-  // // Temporarily until we update the guides
-  // packages['@angular/router-deprecated'] = { main: '/bundles/router-deprecated' + '.umd.js', defaultExtension: 'js' };
-  //
-  // var config = {
-  //   // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
-  //   transpiler: 'ts',
-  //   typescriptOptions: {
-  //     tsconfig: true
-  //   },
-  //   meta: {
-  //     'typescript': {
-  //       "exports": "ts"
-  //     }
-  //   },
-  //   map: map,
-  //   packages: packages
-  // };
-  //
-  // System.config(config);
 
   System.config({
     transpiler: 'ts',
